b59fea57939704f9ca1266a410eb3603917d5627
TJ Momon <tjmomon92@gmail.com>
2023-04-20 11:36:11 -0500

    set up base file structure

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,19 @@ const fs = require('fs');
 const readMe = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
+const questions = [
+
+]
 
 // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
