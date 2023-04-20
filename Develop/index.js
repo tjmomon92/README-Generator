7dc70128db8395b5ac8f8d638d6f71e4210ad4e1
TJ Momon <tjmomon92@gmail.com>
2023-04-20 12:09:53 -0500

    added in README questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,18 +5,66 @@ const readMe = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-
-]
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'Name of project?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Description of project?',
+    },
+    {
+        type: 'input',
+        name: 'Install',
+        message: 'Install requirements?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Project use description?',
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'How to contribute?',
+    },
+    {
+        type: 'input',
+        name: 'Test',
+        message: 'Project testing?',
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'Desired license?',
+        choices: ['Apache', 'Boost', 'Eclipse', 'IBM', 'ISC', 'MIT', 'Mozilla', 'GPL-v2', 'GPL-v3', 'GPL-LGPL'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'GitHub profile?',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'Email Address?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
-}
+};
 
 // TODO: Create a function to initialize app
 function init() {
 
-}
+};
 
 // Function call to initialize app
 init();
