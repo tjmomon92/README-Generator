681795b15e9291f4f337486c1aea836e943931d4
TJ Momon <tjmomon92@gmail.com>
2023-04-20 11:18:42 -0500

    added index.js file and package.json file

---
+++ Develop/index.js
@@ -0,0 +1,10 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const readMe = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+
+// TODO: Create a function to write README file
+
+// TODO: Create a function to initialize app
